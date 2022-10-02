399a0acede33ec0f01ff4206cb24792b986885a8
mohadmin25 <abuhamar008@gmail.com>
2022-10-02 23:19:47 +0300

    done

--- src/components/App.js
+++ src/components/App.js
@@ -16,16 +16,26 @@ function Home() {
   );
 }
 
+function About(){
+  return (
+    <div id="about" >
+      <h2>About</h2>
+    </div>
+  )
+}
+
 {/* write an <About> component here */}
 
 
 function App() {
   return (
-    <div>
-      <NavBar />
-      {/* add the <Home> component here */}
-      {/* add your <About> component here */}
-    </div>
+    <>
+      <div>
+        <NavBar />
+        <Home />
+        <About/>
+      </div>
+    </>
   );
 }
 
